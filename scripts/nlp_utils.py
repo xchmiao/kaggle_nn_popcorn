68bf5bf207a86ecd70644f71ba2695f5aa2d7046
Xiaochang Miao <Angela@Xiaochangs-MacBook-Pro.local>
2018-09-26 21:27:39 -0700

    revised nlp_utils.py

--- scripts/nlp_utils.py
+++ scripts/nlp_utils.py
@@ -11,16 +11,16 @@ class Preprocess(object):
     def __init__(self):
         pass
 
-    def _html_to_text(self, review):
+    def html_to_text(self, review):
         review_text = BeautifulSoup(review, "html").get_text()
         if len(review_text) < 1:
             review_text = review
         return review_text
 
-    def _remove_non_letters(self, text):
+    def remove_non_letters(self, text):
         return re.sub("[^a-zA-Z]", " ", review_text)
 
-    def _remove_sw(self, word_list):
+    def remove_sw(self, word_list):
         '''
         Remove stop words from the input word list
         :param word_list: list of strings
@@ -40,14 +40,13 @@ class Preprocess(object):
         :return:
         '''
 
-        # 1. Remove Html
-        review_text = self._html_to_text(review)
-        review_text = self._remove_non_letters(review_text) # remove non-letters
+        review_text = self.html_to_text(review)
+        review_text = self.remove_non_letters(review_text) # remove non-letters
 
         words = review_text.lower().split()
 
         if remove_stopwords:
-            words = self._remove_sw(words)
+            words = self.remove_sw(words)
 
         if remove_numbers:
             words = filter(lambda x: x.isalpha(), words)
@@ -56,7 +55,7 @@ class Preprocess(object):
 
     def review_to_sentences(self, review, remove_stopwords = True, remove_numbers = False):
 
-        review_text = self._html_to_text(review)
+        review_text = self.html_to_text(review)
         raw_sentences = nltk.sent_tokenize(review_text.strip())
 
         sentences = []
@@ -68,29 +67,29 @@ class Preprocess(object):
 
     def clean_review_tfidf(self, remove_stopwords = True, remove_numbers = True):
         '''
+        Clean the review text by converting it to html, tokenizing to sentences, removing non-letters,
+        and lemmatization, and make it ready to extract TF-IDF features.
 
-        :param remove_stopwords:
-        :param remove_numbers:
-        :return:
+        :param remove_stopwords: boolean
+        :param remove_numbers: boolean
+        :return: doc: list of lists, cleaned sentences in word-tokens
         '''
 
-        review_text = BeautifulSoup(review, "html").get_text()
-        if len(review_text) < 1:
-            review_text = review
+        review_text = self.html_to_text(review)
         sentences = nltk.tokenize.sent_tokenize(review_text)
 
-        sentences = map(lambda s: re.sub("[^a-zA-Z\s]", "", s), sentences) #remove non-letter, but keep orginal spaces
+        sentences = list(map(self.remove_non_letters, sentences)) #remove non-letter, but keep orginal spaces
 
-        sentences = map(lambda s: s.lower().split(), sentences)
+        sentences = list(map(lambda s: s.lower().split(), sentences))
 
-        sentences_tagged = map(lambda s: nltk.pos_tag(s), sentences)
+        sentences_tagged = list(map(lambda s: nltk.pos_tag(s), sentences))
 
         if remove_stopwords:
             stops = set(stopwords.words("english"))
-            sentences_tagged = map(lambda l: filter(lambda x: x[0] not in stops, l), sentences_tagged)
+            sentences_tagged = list(map(lambda l: list(filter(lambda x: x[0] not in stops, l)), sentences_tagged))
 
         if remove_numbers:
-            sentences_tagged = map(lambda l: filter(lambda x: x[0].isalpha(), l), sentences_tagged)
+            sentences_tagged = list(map(lambda l: list(filter(lambda x: x[0].isalpha(), l)), sentences_tagged))
 
         # lemmatization
         tag_map = defaultdict(lambda: wordnet.NOUN)
@@ -102,7 +101,7 @@ class Preprocess(object):
 
         doc = []
         for sent in sentences_tagged:
-            s = map(lambda (word, tag): lemma.lemmatize(word, pos=tag_map[tag[0]]), sent)
+            s = list(map(lambda (word, tag): lemma.lemmatize(word, pos=tag_map[tag[0]]), sent))
             doc.append(" ".join(s))
 
         return doc
